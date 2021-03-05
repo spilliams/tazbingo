664e2116ae24d49230d047efc7d3962ed8dbc84c
Spencer Williams <s@spencerenglish.com>
2021-03-05 12:33:42 -0800

    typo

--- bingo.js
+++ bingo.js
@@ -3,7 +3,7 @@ window.onresize = resize;
 
 function airtableTiles(view) {
     var Airtable = require('airtable');
-    var db = new Airtable({apiKey: 'keyqw38Y5vuJlSnaB'}).base('appjKWzB7gDN1NDgm');
+    var db = new Airtable({ apiKey: 'keyqw38Y5vuJlSnaB' }).base('appjKWzB7gDN1NDgm');
     return db('Table 1').select({
         fields: ["HTML", "Contributor"],
         view: view,
@@ -34,24 +34,24 @@ function resize() {
 
     // colophon is below the fold, and is the final 10% of the height
     colophonP = 10;
-    finalH = Math.round(h / ((100-colophon)/100));
+    finalH = Math.round(h / ((100 - colophonP) / 100));
     w = Math.round(w);
     // console.log(`card is ${finalH} by ${w}`);
     $("#card").css({ "width": "" + w + "px" }); // "height": "" + finalH + "px", 
 
     // bingo is square
     // console.log(`bingo is ${w} high`);
-    $("#bingo").css({"height": ""+w+"px"});
+    $("#bingo").css({ "height": "" + w + "px" });
 
     // title is the remainder of the original aspect ratio, minus 2 points
     titleP = ((aD - aN) / aD) - 0.02;
-    colophonP = (colophonP+2)/100.0;
+    colophonP = (colophonP + 2) / 100.0;
     // console.log(`title is ${title}% and colophon is ${colophon}%`);
     $("#title").css({
-        "height": `${titleP*h}px`,
-        "line-height": `${titleP*h*0.95}px` // 0.95 is line-height from css
+        "height": `${titleP * h}px`,
+        "line-height": `${titleP * h * 0.95}px` // 0.95 is line-height from css
     });
-    $("#by").css({"height": `${colophonP*h}px`});
+    $("#by").css({ "height": `${colophonP * h}px` });
 }
 
 function another() {
@@ -61,30 +61,30 @@ function another() {
     // to database?
     contributors = ["@jrfbz", "@spilliams", "@jeslach"];
     defaultRegularTiles = [
-        {"fields": {"HTML": "Fantasy merchant cat", "Contributor": "@jrfbz"}},
-        {"fields": {"HTML": "OP queer mage", "Contributor": "@jrfbz"}},
-        {"fields": {"HTML": "Competent woman who can bail the PCs out", "Contributor": "@jrfbz"}},
-        {"fields": {"HTML": "Fixation on a<br>single type of food", "Contributor": "@jrfbz"}},
-        {"fields": {"HTML": "Vacation episode", "Contributor": "@jrfbz"}},
-        {"fields": {"HTML": "Monster made out of other monsters", "Contributor": "@jrfbz"}},
-        {"fields": {"HTML": "Clint sings", "Contributor": "@jrfbz"}},
-        {"fields": {"HTML": "Flirting with<br>monsters", "Contributor": "@jrfbz"}},
-        {"fields": {"HTML": "Travis has a pet", "Contributor": "@jrfbz"}},
-        {"fields": {"HTML": "Extreme sports", "Contributor": "@jrfbz"}},
-        {"fields": {"HTML": "Celebrity appearance", "Contributor": "@jrfbz"}},
-        {"fields": {"HTML": "Techni&shy;cally, aliens", "Contributor": "@jrfbz"}},
-        {"fields": {"HTML": "Elevators", "Contributor": "@jrfbz"}},
-        {"fields": {"HTML": "Friendship is magic", "Contributor": "@jrfbz"}},
-        {"fields": {"HTML": "Under&shy;staffed<br>secret organization", "Contributor": "@jrfbz"}},
-        {"fields": {"HTML": "Justin voices his own<br>sidekick", "Contributor": "@jrfbz"}},
-        {"fields": {"HTML": "Candle&shy;nights episode", "Contributor": "@jrfbz"}},
-        {"fields": {"HTML": "Griffin flakes out on a<br>character voice", "Contributor": "@jrfbz"}},
-        {"fields": {"HTML": "Planet-destroying force", "Contributor": "@jrfbz"}},
-        {"fields": {"HTML": "Lunar<br>interludes", "Contributor": "@jeslach"}},
-        {"fields": {"HTML": "Ghosts", "Contributor": "@jrfbz"}},
-        {"fields": {"HTML": "PC loses their<br>powers", "Contributor": "@jrfbz"}},
-        {"fields": {"HTML": "Chekhov's party<br>member", "Contributor": "@jrfbz"}},
-        {"fields": {"HTML": "Catch&shy;phrases", "Contributor": "@spilliams"}},
+        { "fields": { "HTML": "Fantasy merchant cat", "Contributor": "@jrfbz" } },
+        { "fields": { "HTML": "OP queer mage", "Contributor": "@jrfbz" } },
+        { "fields": { "HTML": "Competent woman who can bail the PCs out", "Contributor": "@jrfbz" } },
+        { "fields": { "HTML": "Fixation on a<br>single type of food", "Contributor": "@jrfbz" } },
+        { "fields": { "HTML": "Vacation episode", "Contributor": "@jrfbz" } },
+        { "fields": { "HTML": "Monster made out of other monsters", "Contributor": "@jrfbz" } },
+        { "fields": { "HTML": "Clint sings", "Contributor": "@jrfbz" } },
+        { "fields": { "HTML": "Flirting with<br>monsters", "Contributor": "@jrfbz" } },
+        { "fields": { "HTML": "Travis has a pet", "Contributor": "@jrfbz" } },
+        { "fields": { "HTML": "Extreme sports", "Contributor": "@jrfbz" } },
+        { "fields": { "HTML": "Celebrity appearance", "Contributor": "@jrfbz" } },
+        { "fields": { "HTML": "Techni&shy;cally, aliens", "Contributor": "@jrfbz" } },
+        { "fields": { "HTML": "Elevators", "Contributor": "@jrfbz" } },
+        { "fields": { "HTML": "Friendship is magic", "Contributor": "@jrfbz" } },
+        { "fields": { "HTML": "Under&shy;staffed<br>secret organization", "Contributor": "@jrfbz" } },
+        { "fields": { "HTML": "Justin voices his own<br>sidekick", "Contributor": "@jrfbz" } },
+        { "fields": { "HTML": "Candle&shy;nights episode", "Contributor": "@jrfbz" } },
+        { "fields": { "HTML": "Griffin flakes out on a<br>character voice", "Contributor": "@jrfbz" } },
+        { "fields": { "HTML": "Planet-destroying force", "Contributor": "@jrfbz" } },
+        { "fields": { "HTML": "Lunar<br>interludes", "Contributor": "@jeslach" } },
+        { "fields": { "HTML": "Ghosts", "Contributor": "@jrfbz" } },
+        { "fields": { "HTML": "PC loses their<br>powers", "Contributor": "@jrfbz" } },
+        { "fields": { "HTML": "Chekhov's party<br>member", "Contributor": "@jrfbz" } },
+        { "fields": { "HTML": "Catch&shy;phrases", "Contributor": "@spilliams" } },
     ];
 
     Promise.all([airtableTiles("Live Regular Spaces"), airtableTiles("Live Free Spaces")]).then(([regularTiles, freeTiles]) => {
@@ -106,7 +106,7 @@ function another() {
         if (typeof freeTiles != "undefined") {
             freeTiles = randomTiles(1, freeTiles);
             tile = freeTiles[0].fields;
-            $("#free").html(tile.HTML+"<br>(Free Space)");
+            $("#free").html(tile.HTML + "<br>(Free Space)");
             contributors.push(tile.Contributor);
         }
 
